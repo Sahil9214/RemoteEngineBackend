164409c87ff8d9052320f7815e37488add4b7f1a
Utkarsh <utkarshsinghal369@gmail.com>
2023-12-17 13:05:12 +0530

    final submission

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 const express = require("express");
-const mongoose = require("mongoose");
+require("dotenv").config()
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const { DeveloperRouter } = require("./routes/Developer.routes");
 const { clientRouter } = require("./routes/Client.routes");
 const { isAuth } = require("./Middleware/auth.Middleware");
+const {connection}=require('./db')
 
 const app = express();
 
 
 
 app.use(express.json());
-app.use(cookieParser());
+
 app.use(cors());
 
-app.use("/api/developers", isAuth, DeveloperRouter);
-app.use("/api/clients", clientRouter);
+app.use("/developers", isAuth, DeveloperRouter);
+app.use("/clients", clientRouter);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+const PORT = process.env.PORT ;
+app.listen(PORT, async(res,req) => {
+  try{
+await connection;
+console.log("bhai acha mai chalta hu")
+  }
+  catch(err){
+    console.log(`Server is running on port ${PORT}`);
+  }
 });
--- routes/Client.routes.js
+++ routes/Client.routes.js
@@ -9,11 +9,17 @@ clientRouter.post("/register", async (req, res) => {
 
   try {
     bcrypt.hash(password, 5, async (err, hash) => {
+      if (err) {
+        console.error("Bcrypt hash error:", err);
+        throw err; // Throw the error to be caught in the catch block
+      }
+
       const user = new ClientModel({ email, password: hash });
       await user.save();
       res.status(201).send({ msg: "You successfully registered" });
     });
   } catch (err) {
+    console.error("Register error:", err);
     res.status(400).send({ msg: "Failed to register" });
   }
 });
@@ -27,7 +33,10 @@ clientRouter.post("/login", async (req, res) => {
     if (user) {
       bcrypt.compare(password, user.password, (err, result) => {
         if (result === true) {
-          const token = jwt.sign({ userId: user._id, userEmail: user.email }, process.env.JWT_SECRET);
+          const token = jwt.sign(
+            { userId: user._id, userEmail: user.email },
+            process.env.JWT_SECRET
+          );
           res.status(200).send({ msg: "Successfully obtained token", token });
         } else {
           res.status(401).send({ msg: "Invalid credentials" });
