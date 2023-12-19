b386ed4387665630b0b2df01eac60221ac4438da
Utkarsh <utkarshsinghal369@gmail.com>
2023-12-19 18:06:29 +0530

    change

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(cors());
 
-app.use("/developers", DeveloperRouter);
+app.use("/developers",DeveloperRouter);
 app.use("/clients", clientRouter);
 
 const PORT = process.env.PORT||3000 ;
